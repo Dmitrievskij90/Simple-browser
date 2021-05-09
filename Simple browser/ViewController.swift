f17b474cfe3ec9521043e38cf38ecd09a53acf8b
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-09 18:44:23 +0600

    add func loadDefaultPage

--- Simple browser/ViewController.swift
+++ Simple browser/ViewController.swift
@@ -12,17 +12,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var seachBar: UISearchBar!
     @IBOutlet weak var webView: WKWebView!
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.navigationDelegate = self
+
+        loadDefaultPage()
     }
 
-    @IBAction func backButtonPressed(_ sender: UIBarButtonItem) {
+    @IBAction private func backButtonPressed(_ sender: UIBarButtonItem) {
     }
 
-    
-    @IBAction func nextButtonPressed(_ sender: Any) {
+    @IBAction private func nextButtonPressed(_ sender: Any) {
     }
-    @IBAction func refreshButtonPressed(_ sender: UIBarButtonItem) {
+
+    @IBAction private func refreshButtonPressed(_ sender: UIBarButtonItem) {
     }
+
+    private func loadDefaultPage() {
+        guard let url = URL(string: "https://www.google.com") else {
+            return
+        }
+        let requesr = URLRequest(url: url)
+        webView.load(requesr)
+    }
+}
+
+extension ViewController: WKNavigationDelegate {
 }
