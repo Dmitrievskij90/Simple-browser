037f4d4ec526a6761bc8600a36fc44999c4c9bbe
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-09 18:40:49 +0600

    add outlets and actions

--- Simple browser/ViewController.swift
+++ Simple browser/ViewController.swift
@@ -6,10 +6,23 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var seachBar: UISearchBar!
+    @IBOutlet weak var webView: WKWebView!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+    }
+
+    @IBAction func backButtonPressed(_ sender: UIBarButtonItem) {
+    }
+
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+    }
+    @IBAction func refreshButtonPressed(_ sender: UIBarButtonItem) {
     }
 }
