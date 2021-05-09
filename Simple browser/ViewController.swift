bfa0cab7ccc095798e5d98c466bf630280131ea5
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-05-09 18:22:21 +0600

    Initial commit

---
+++ Simple browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple browser
+//
+//  Created by Konstantin Dmitrievskiy on 09.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
